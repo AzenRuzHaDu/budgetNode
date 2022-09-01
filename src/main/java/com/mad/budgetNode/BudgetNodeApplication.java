8182ebd4292fe17d57159311b10b4f2dec50597b
Mad <madiane.lenigen@protonmail.com>
2022-09-01 21:53:36 +0200

    initializing the git repo

---
+++ src/main/java/com/mad/budgetNode/BudgetNodeApplication.java
@@ -0,0 +1,13 @@
+package com.mad.budgetNode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetNodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mad/budgetNode/BudgetNodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mad.budgetNode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetNodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
